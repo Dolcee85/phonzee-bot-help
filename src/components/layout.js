5ddad450aaa0d09250b0f0789c5d4bf870425c64
Dolcee85 <dolceeghost85@gmail.com>
2022-11-13 03:43:34 -0500

    Completed basic layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,17 +4,29 @@ import icon from '../images/icon.png';
 
 export default function Layout ({ title, children}) {
   return (
-    <div class="main-container">
-      <div class="content-title">
+    <div class="parent-container">
+      <div class="title-container">
         <img class="icon" src={icon}/>
         <h2>Phonzee Bot Docs</h2>
       </div>
-      
-      <div class="content-header">
-        <h2>{ title }</h2>
+
+      <div class="content-navi-top">
+        <div class="content-navi-item">
+          content-navi-top
+        </div>
       </div>
-      <div class="content-body">
-        { children }
+      
+      <div class="content-navi-side">
+        <div class="content-navi-item">
+          content-navi-side
+        </div>
+      </div>
+
+      <div class="content-container">
+        <h2>{ title }</h2>
+        <div class="content-body">
+          { children }
+        </div>
       </div>
 
     </div>
