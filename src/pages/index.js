7634692d56558c65ca18fbed550fcb5c8f0b801e
Dolcee85 <dolceeghost85@gmail.com>
2022-11-15 02:26:58 -0500

    Added command info for events and scores

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
         <div class="command-description">
           <p>
-            Displays the latest match events for a given club.
+            Displays the match score, status, details, and events for a given club.
           </p>
           <p>
             <code class="code-arg-required">league</code> The league the club is in. Check supported 
@@ -40,7 +40,37 @@ export default function Home() {
             can be looked up.
           </p>
           <p>
-            <code class="code-example">z-ev mls tor 04-24</code>
+            <code class="code-example">z-ev mls tor 04-24</code> displays the match score, status, 
+            details, and match events for an MLS match played by Toronto FC on April 24th.
+          </p>
+        </div>
+      </div>
+
+      <div class="command-container">
+        <div class="command-name">
+          <p class="command-name-title">
+            <h3>Scores</h3>
+            <code>s</code>, <code>scores</code>
+          </p>
+          <p>
+            <code>s <span class="code-arg-required">&lt;league&gt;</span> &lt;range&gt;</code>
+          </p>
+        </div>
+        <div class="command-description">
+          <p>
+            Displays the scores for a given league. Check supported leagues.
+          </p>
+          <p>
+            <code class="code-arg-required">league</code> The league to display scores for.<br/>
+            <code class="code-arg-required">range</code> Determine the schedule range of the scores: <br/>
+            <div class="command-description-sub">
+              <code class="code-generic">previous</code>/<code class="code-generic">p</code> Check scores from previous days, up to a week.<br/>
+              <code class="code-generic">current</code>/<code class="code-generic">c</code> Check most recent scores, including scores from yesterday and for tomorrow.<br/>
+              <code class="code-generic">next</code>/<code class="code-generic">n</code> Check upcoming matches, up to a week.
+            </div>
+          </p>
+          <p>
+            <code class="code-example">z-s mls p</code> shows previously played MLS matches.
           </p>
         </div>
       </div>
