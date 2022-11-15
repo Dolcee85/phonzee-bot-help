b96c885b8320b4302bd8bac2472b87c47c4cf662
Dolcee85 <dolceeghost85@gmail.com>
2022-11-15 02:07:31 -0500

    Switched to flexbox for command container

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,42 +17,33 @@ export default function Home() {
         <b>Phonzee's prefix: <code>z-</code></b>
       </p>
       
-      <p>
-        <table>
-          {/* <thead>
-            <tr>
-              <th>Command</th>
-              <th>Description</th>
-            </tr>
-          </thead> */}
-          <tbody>
-            <tr>
-              <td>
-                <b>Events</b><br/>
-                <code>ev</code><br/>
-                <code>boxscore</code><br/>
-                <code>bs</code><br/>
-              </td>
-              <td>
-                Displays the latest match events for a given club.<br/>
-                <code><span class="code-arg-required">&lt;league&gt; &lt;club&gt;</span> &lt;date&gt;</code><br/>
-                Example: <code><span class="code-example">z-ev mls tor 04-24</span></code>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <b>Scores</b><br/>
-                <code>s</code><br/>
-                <code>scores</code><br/>
-              </td>
-              <td>
-                Displays the scores for a given league.<br/>
-                <code>s &lt;league&gt; &lt;range&gt;</code>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </p>
+      <div class="command-container">
+        <div class="command-name">
+          <p class="command-name-title">
+            <h3>Events</h3>
+            <code>ev</code>, <code>boxscore</code>, <code>bs</code>
+          </p>
+          <p>
+            <code>ev <span class="code-arg-required">&lt;league&gt;</span> <span class="code-arg-required">&lt;club&gt;</span> &lt;date&gt;</code>
+          </p>
+        </div>
+        <div class="command-description">
+          <p>
+            Displays the latest match events for a given club.
+          </p>
+          <p>
+            <code class="code-arg-required">league</code> The league the club is in. Check supported 
+            leagues.<br/>
+            <code class="code-arg-required">club</code> The tri-code of the club.<br/>
+            <code>date</code> The date of the match in <code class="code-generic">MM-dd</code> format. If this is not
+            supplied, the most recent match is chosen. Any match that shows up in the Score command 
+            can be looked up.
+          </p>
+          <p>
+            <code class="code-example">z-ev mls tor 04-24</code>
+          </p>
+        </div>
+      </div>
 
     </Layout>
   );
