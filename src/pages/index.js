41f50b24eec990bb72c3e38f1d6066da4341ad2e
Dolcee85 <dolceeghost85@gmail.com>
2022-11-17 03:34:00 -0500

    Made more changes to command page layout and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,10 +38,10 @@ export default function Home() {
             Displays the match score, status, details, and events for a given club.
           </p>
           <p>
-            <code class="code-arg-required">league</code> The league the club is in. Check supported 
+            <code class="code-arg-required">league</code>: The league the club is in. Check supported 
             leagues.<br/>
-            <code class="code-arg-required">club</code> The tri-code of the club.<br/>
-            <code>date</code> The date of the match in <code class="code-generic">MM-dd</code> format. If this is not
+            <code class="code-arg-required">club</code>: The tri-code of the club.<br/>
+            <code>date</code>: The date of the match in <code class="code-generic">MM-dd</code> format. If this is not
             supplied, the most recent match is chosen. Any match that shows up in the Score command 
             can be looked up.
           </p>
@@ -69,7 +69,7 @@ export default function Home() {
             Send feedback to help improve Phonzee!
           </p>
           <p>
-            <code class="code-arg-required">message</code> The feedback message.
+            <code class="code-arg-required">message</code>: The feedback message.
           </p>
           <p>
             <code class="code-example">z-fb mls scores arent showing properly</code>
@@ -151,12 +151,12 @@ export default function Home() {
             Displays the scores for a given league.
           </p>
           <p>
-            <code class="code-arg-required">league</code> The league to display scores for. Check supported leagues.<br/>
-            <code>range</code> Determine the schedule range of the scores: <br/>
+            <code class="code-arg-required">league</code>: The league to display scores for. Check supported leagues.<br/>
+            <code>range</code>: Determine the schedule range of the scores: <br/>
             <div class="command-description-sub">
-              <code class="code-generic">previous</code> or <code class="code-generic">p</code> Get scores from previous days, up to a week.<br/>
-              <code class="code-generic">current</code> or <code class="code-generic">c</code> Get most recent scores, including scores for today, from yesterday, and for tomorrow. The default range.<br/>
-              <code class="code-generic">next</code> or <code class="code-generic">n</code> Get upcoming matches, up to a week.
+              <code class="code-generic">previous</code> or <code class="code-generic">p</code>: Get scores from previous days, up to a week.<br/>
+              <code class="code-generic">current</code> or <code class="code-generic">c</code>: Get most recent scores, including scores for today, from yesterday, and for tomorrow. The default range.<br/>
+              <code class="code-generic">next</code> or <code class="code-generic">n</code>: Get upcoming matches, up to a week.
             </div>
             If range is not given, it will get most recent scores.
           </p>
@@ -184,9 +184,9 @@ export default function Home() {
             given match.
           </p>
           <p>
-            <code class="code-arg-required">league</code> The league the club is in. Check supported leagues.<br/>
-            <code class="code-arg-required">club</code> The tri-code of the club.<br/>
-            <code>date</code> The date of the match in <code class="code-generic">MM-dd</code> format. If this is not
+            <code class="code-arg-required">league</code>: The league the club is in. Check supported leagues.<br/>
+            <code class="code-arg-required">club</code>: The tri-code of the club.<br/>
+            <code>date</code>: The date of the match in <code class="code-generic">MM-dd</code> format. If this is not
             supplied, the most recent match is chosen. Any match that shows up in the Score command 
             can be looked up.
           </p>
