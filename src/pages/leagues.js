e83e73749cbf7cfdec28cd9c609aef08d1525614
Dolcee85 <dolceeghost85@gmail.com>
2022-11-17 00:07:06 -0500

    Added page for supported leagues

---
+++ src/pages/leagues.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+export default function Home() {
+  return (
+    <Layout title="Supported Leagues">
+
+    </Layout>
+  );
+}
+
+/**
+ * 
+ * T-∞ ヽ༼ຈل͜ຈ༽ﾉ 
+ * 
+ */
