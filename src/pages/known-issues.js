ed74eb7dd5653c93310c8987975d10ca64e1354c
Dolcee85 <dolceeghost85@gmail.com>
2022-12-07 21:50:46 -0500

    Added an issue under known issues

--- src/pages/known-issues.js
+++ src/pages/known-issues.js
@@ -5,7 +5,22 @@ import Layout from '../components/layout';
 export default function Home() {
   return (
     <Layout title="Known Issues">
-      Known issues
+      <thead>
+        <tr>
+          <th>Issue No.</th>
+          <th>Description</th>
+          <th>Command</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Live scores for Canadian Premier League games are not working properly.</td>
+          <td><code>z-s cpl</code></td>
+          <td>Not fixed</td>
+        </tr>
+      </tbody>
     </Layout>
   );
 }
