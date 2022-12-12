af0f493e9d7df047672121d69cf5f3151ac1c1d2
Dolcee85 <dolceeghost85@gmail.com>
2022-12-11 20:24:42 -0500

    Added date for known issue

--- src/pages/known-issues.js
+++ src/pages/known-issues.js
@@ -4,23 +4,31 @@ import Layout from '../components/layout';
 
 export default function Home() {
   return (
+
+    /*  Format for issues:
+          * Issue title
+            * Discovery Date:
+            * In command:
+            * Description:
+            * Status:
+    */
     <Layout title="Known Issues">
-      <thead>
-        <tr>
-          <th>Issue No.</th>
-          <th>Description</th>
-          <th>Command</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Live scores for Canadian Premier League games are not working properly.</td>
-          <td><code>z-s cpl</code></td>
-          <td>Not fixed</td>
-        </tr>
-      </tbody>
+      <div class="issue-container">
+        <p>
+          Live scores for Canadian Premier League games are not working properly.
+        </p>
+        <div class="issue-desc">
+          <ul>
+            <li><b>Discovery Date:</b> 02 May 2022</li>
+            <li><b>In command:</b> <code>z-s cpl</code></li>
+            <li>
+              <b>Description:</b> After executing the <code>z-s cpl</code> command, games within the 
+              specified range will show, but live scores and results will not.
+            </li>
+            <li><b>Status:</b> Not fixed</li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
