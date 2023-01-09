0ace3a1730b6b8ffe0997197225fcc62685d601b
Dolcee85 <dolceeghost85@gmail.com>
2023-01-08 21:20:27 -0500

    Updated known issues page

--- src/pages/known-issues.js
+++ src/pages/known-issues.js
@@ -29,6 +29,27 @@ export default function Home() {
           </ul>
         </div>
       </div>
+
+      <div class="issue-container">
+        <p class="issue-title">
+          ⚠️ If there are no games for today but there are games for tomorrow, 
+          the <code>z-s</code> command will not show them.
+        </p>
+        <div class="issue-desc">
+          <ul>
+            <li><b>Discovery Date:</b> 07 Jan 2023</li>
+            <li><b>In command:</b> <code>z-s</code></li>
+            <li>
+              <b>Description:</b> The <code>z-s</code> command was changed so 
+              that <code>today</code> is the default argument if the user did not pass their own 
+              argument. If there are no games for today, then the next argument used 
+              is <code>upcoming</code>. However, given the logic for that argument, if there are 
+              games listed for tomorrow, they will not show.
+            </li>
+            <li><b>Status:</b> ❌ Not fixed</li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
